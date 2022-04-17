ddf21406597e2c6ed0ae61116f79d7ddedfda24b
nevenxr <nevenxr@gmail.com>
2022-04-17 21:26:37 +0600

    refactor(slashCommands): add permission check

--- src/events/slashCommands.js
+++ src/events/slashCommands.js
@@ -3,6 +3,10 @@ module.exports = {
     exec: (interaction) => {
         if (!interaction.isCommand()) return;
 
+        if (!interaction.guild.me.permissions.has([
+            "SEND_MESSAGES", "EMBED_LINKS"
+        ])) return;
+
         const { commandName } = interaction;
         const command = global.client.commands.get(commandName);
 
